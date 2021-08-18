db53cda6da001a82e2c7498003f7ef5dd3ec859a
GNUOverYooouu <dtortoise99@gmail.com>
2021-08-18 13:44:56 -0700

    cdull.h

---
+++ src/include/cdull.h
@@ -0,0 +1,8 @@
+#ifndef __CDULL_H__
+#define __CDULL_H__
+
+#include <stdio.h>
+
+int cdull(char usage[], char args[]);
+
+#endif
--- src/include/main.h
+++
@@ -1,8 +0,0 @@
-#ifndef __MAIN_H__
-#define __MAIN_H__
-
-#include <stdio.h>
-
-int main(void);
-
-#endif
